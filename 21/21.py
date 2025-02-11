8ec22d391a9783990e24626a34d367583f8ecc16
Mason Pimentel <me@masonpimentel.com>
2025-02-10 21:13:10 -0800

    Day 21 part 2 😭

--- 21/21.py
+++ 21/21.py
@@ -1,254 +1,239 @@
 from sys import maxsize
+from os.path import join
 
-number_pad = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['X', '0', 'A'],
-]
+class Day01:
+    def __init__(self):
+        self.number_pad = [
+            ['7', '8', '9'],
+            ['4', '5', '6'],
+            ['1', '2', '3'],
+            ['X', '0', 'A'],
+        ]
 
-NUMBER_PAD_ROWS = len(number_pad)
-NUMBER_PAD_COLS = len(number_pad[0])
+        self.NUMBER_PAD_ROWS = len(self.number_pad)
+        self.NUMBER_PAD_COLS = len(self.number_pad[0])
 
-row_col_from_num_pad: dict[str, tuple[int, int]] = {
-    number_pad[row][col]: (row, col)
-    for row in range(NUMBER_PAD_ROWS)
-    for col in range(NUMBER_PAD_COLS)
-}
+        self.row_col_from_num_pad: dict[str, tuple[int, int]] = {
+            self.number_pad[row][col]: (row, col)
+            for row in range(self.NUMBER_PAD_ROWS)
+            for col in range(self.NUMBER_PAD_COLS)
+        }
 
-direction_pad = [
-    ['X', '^', 'A'],
-    ['<', 'v', '>']
-]
+        self.direction_pad = [
+            ['X', '^', 'A'],
+            ['<', 'v', '>']
+        ]
 
-DIRECTION_PAD_ROWS = len(direction_pad)
-DIRECTION_PAD_COLS = len(direction_pad[0])
+        self.DIRECTION_PAD_ROWS = len(self.direction_pad)
+        self.DIRECTION_PAD_COLS = len(self.direction_pad[0])
 
-row_col_from_dir_pad: dict[str, tuple[int, int]] = {
-    direction_pad[row][col]: (row, col)
-    for row in range(DIRECTION_PAD_ROWS)
-    for col in range(DIRECTION_PAD_COLS)
-}
+        self.row_col_from_dir_pad: dict[str, tuple[int, int]] = {
+            self.direction_pad[row][col]: (row, col)
+            for row in range(self.DIRECTION_PAD_ROWS)
+            for col in range(self.DIRECTION_PAD_COLS)
+        }
 
-def dir_pad_same_level(src: str, dst: str, seen: set[str]) -> list[tuple[str, int]]:
-    if src in seen or src == 'X':
-        return [('', maxsize)]
+        self.dp: dict[str, dict[str, int]] = {}
+        self.dir_pad_dp: dict[int, dict[str, dict[str, tuple[str, int]]]] = {}
+        self.dir_pad_same_level_dp: dict[str, dict[str, list[tuple[str, int]]]] = {}
 
-    if src == dst:
-        return [('A', 1)]
+    def dir_pad_same_level(self, src: str, dst: str, seen: set[str]) -> list[tuple[str, int]]:
+        if src in seen or src == 'X':
+            return [('', maxsize)]
 
-
-    seen.add(src)
-
-    src_row, src_col = row_col_from_dir_pad[src]
-
-    possible_this_level: list[tuple[str, int]] = []
-
-    for row_diff, col_diff, input in [(-1, 0, '^'), (0, 1, '>'), (1, 0, 'v'), (0, -1, '<')]:
-        check_row = src_row + row_diff
-        check_col = src_col + col_diff
-
-        if check_row < 0 or check_row >= DIRECTION_PAD_ROWS or check_col < 0 or check_col >= DIRECTION_PAD_COLS:
-            continue
-
-        this_move_dst = direction_pad[check_row][check_col]
-
-        this_layer_res = dir_pad_same_level(this_move_dst, dst, seen)
-
-        # print(f'src {src} dst {dst} this_layer_res {this_layer_res}')
-
-        for this_layer_seq, this_layer_dist in this_layer_res:
-            if this_layer_dist < maxsize:
-                possible_this_level.append((input + this_layer_seq, 1 + this_layer_dist))
-
-    seen.remove(src)
-
-    return possible_this_level
-
-def dir_pad_all_levels(src: str, dst: str, level: int, human_level: int) -> tuple[str, int]:
-    if level not in dir_pad_dp:
-        dir_pad_dp[level] = {}
-    if src not in dir_pad_dp[level]:
-        dir_pad_dp[level][src] = {}
-    if dst in dir_pad_dp[level][src]:
-        return dir_pad_dp[level][src][dst]
-
-    if level == human_level:
-        return (dst, 1)
-
-    if src == 'X':
-        return ('', maxsize)
-
-    if src == dst:
-        return ('A', 1)
-
-    if src not in dir_pad_same_level_dp:
-        dir_pad_same_level_dp[src] = {}
-    if dst in dir_pad_same_level_dp[src]:
-        possible_this_level = dir_pad_same_level_dp[src][dst]
-    else:
-        possible_this_level = dir_pad_same_level(src, dst, set())
-        dir_pad_same_level_dp[src][dst] = possible_this_level
-
-    best_distance = maxsize
-    seq = ''
-
-    # print(f'level {level} possible_this_level {possible_this_level}')
-
-    for this_layer_seq, _ in possible_this_level:
-        lower_layers_seq = ''
-        lower_layers_dist = 0
-
-        for i in range(len(this_layer_seq)):
-            lower_dst = this_layer_seq[i]
-            lower_level = level + 1
-
-            if i == 0:
-                lower_src = 'A'
-                lower_layers_res = dir_pad_all_levels('A', this_layer_seq[i], lower_level, human_level)
-            else:
-                lower_src = this_layer_seq[i - 1]
-
-            if lower_level in dir_pad_dp and lower_src in dir_pad_dp[lower_level] and lower_dst in dir_pad_dp[lower_level][lower_src]:
-                lower_layers_res = dir_pad_dp[lower_level][lower_src][lower_dst]
-            else:
-                lower_layers_res = dir_pad_all_levels(lower_src, lower_dst, lower_level, human_level)
-        
-            lower_layers_seq, lower_layers_dist = '', lower_layers_dist + lower_layers_res[1]
-        
-        # print(f'level {level} this_layer_seq {this_layer_seq} lower_layers_seq {lower_layers_seq} lower_layers_dist {lower_layers_dist}')
-
-        if lower_layers_dist < best_distance:
-            best_distance = lower_layers_dist
-            seq = lower_layers_seq
-
-    r = (seq, best_distance)
-    dir_pad_dp[level][src][dst] = r
-    # print(f'level {level} src {src} dst {dst}')
-    return r
+        if src == dst:
+            return [('A', 1)]
 
 
-def number_pad_possible(src: str, dst: str, seen: set[str]) -> list[tuple[str, int]]:
-    if src in seen or src == 'X':
-        return [('', maxsize)]
+        seen.add(src)
 
-    if src == dst:
-        return [('A', 1)]
+        src_row, src_col = self.row_col_from_dir_pad[src]
 
-    seen.add(src)
+        possible_this_level: list[tuple[str, int]] = []
 
-    src_row, src_col = row_col_from_num_pad[src]
+        for row_diff, col_diff, input in [(-1, 0, '^'), (0, 1, '>'), (1, 0, 'v'), (0, -1, '<')]:
+            check_row = src_row + row_diff
+            check_col = src_col + col_diff
 
-    possible_this_level: list[tuple[str, int]] = []
+            if check_row < 0 or check_row >= self.DIRECTION_PAD_ROWS or check_col < 0 or check_col >= self.DIRECTION_PAD_COLS:
+                continue
 
-    for row_diff, col_diff, input in [(-1, 0, '^'), (0, 1, '>'), (1, 0, 'v'), (0, -1, '<')]:
-        check_row = src_row + row_diff
-        check_col = src_col + col_diff
+            this_move_dst = self.direction_pad[check_row][check_col]
 
-        if check_row < 0 or check_row >= NUMBER_PAD_ROWS or check_col < 0 or check_col >= NUMBER_PAD_COLS:
-            continue
+            this_layer_res = self.dir_pad_same_level(this_move_dst, dst, seen)
 
-        this_move_dst = number_pad[check_row][check_col]
+            for this_layer_seq, this_layer_dist in this_layer_res:
+                if this_layer_dist < maxsize:
+                    possible_this_level.append((input + this_layer_seq, 1 + this_layer_dist))
 
-        this_layer_res = number_pad_possible(this_move_dst, dst, seen)
+        seen.remove(src)
 
-        for this_layer_seq, this_layer_dist in this_layer_res:
-            if this_layer_dist < maxsize:
-                possible_this_level.append((input + this_layer_seq, 1 + this_layer_dist))
-
-    seen.remove(src)
-
-    return possible_this_level
-
-
-def number_pad_press(src: str, dst: str, levels: int) -> int:
-    if src not in dp:
-        dp[src] = {}
-    if dst in dp[src]:
-        # print('cached num pad src {src} dst {dst}')
-        return dp[src][dst]
+        return possible_this_level
     
-    possible_ways = number_pad_possible(src, dst, set())
+    def dir_pad_all_levels(self, src: str, dst: str, level: int, human_level: int) -> tuple[str, int]:
+        if level not in self.dir_pad_dp:
+            self.dir_pad_dp[level] = {}
+        if src not in self.dir_pad_dp[level]:
+            self.dir_pad_dp[level][src] = {}
+        if dst in self.dir_pad_dp[level][src]:
+            return self.dir_pad_dp[level][src][dst]
 
-    best_distance = maxsize
-    seq = ''
+        if level == human_level:
+            return (dst, 1)
 
-    for this_layer_seq, _ in possible_ways:
-        lower_layers_seq = ''
-        lower_layers_dist = 0
+        if src == 'X':
+            return ('', maxsize)
 
-        for i in range(len(this_layer_seq)):
-            lower_dst = this_layer_seq[i]
-            if i == 0:
-                lower_src = 'A'
-            else:
-                lower_src = this_layer_seq[i - 1]
-                
-            if 1 in dir_pad_dp and lower_src in dir_pad_dp[1] and lower_dst in dir_pad_dp[1][lower_src]:
-                lower_layers_res = dir_pad_dp[1][lower_src][lower_dst]
-            else:
-                lower_layers_res = dir_pad_all_levels(lower_src, lower_dst, 1, levels)
+        if src == dst:
+            return ('A', 1)
+
+        if src not in self.dir_pad_same_level_dp:
+            self.dir_pad_same_level_dp[src] = {}
+        if dst in self.dir_pad_same_level_dp[src]:
+            possible_this_level = self.dir_pad_same_level_dp[src][dst]
+        else:
+            possible_this_level = self.dir_pad_same_level(src, dst, set())
+            self.dir_pad_same_level_dp[src][dst] = possible_this_level
+
+        best_distance = maxsize
+        seq = ''
+
+        for this_layer_seq, _ in possible_this_level:
+            lower_layers_seq = ''
+            lower_layers_dist = 0
+
+            for i in range(len(this_layer_seq)):
+                lower_dst = this_layer_seq[i]
+                lower_level = level + 1
+
+                if i == 0:
+                    lower_src = 'A'
+                    lower_layers_res = self.dir_pad_all_levels('A', this_layer_seq[i], lower_level, human_level)
+                else:
+                    lower_src = this_layer_seq[i - 1]
+
+                if lower_level in self.dir_pad_dp and lower_src in self.dir_pad_dp[lower_level] and lower_dst in self.dir_pad_dp[lower_level][lower_src]:
+                    lower_layers_res = self.dir_pad_dp[lower_level][lower_src][lower_dst]
+                else:
+                    lower_layers_res = self.dir_pad_all_levels(lower_src, lower_dst, lower_level, human_level)
+            
+                lower_layers_seq, lower_layers_dist = '', lower_layers_dist + lower_layers_res[1]
+            
+            if lower_layers_dist < best_distance:
+                best_distance = lower_layers_dist
+                seq = lower_layers_seq
+
+        r = (seq, best_distance)
+        self.dir_pad_dp[level][src][dst] = r
+        return r
+    
+    def number_pad_possible(self, src: str, dst: str, seen: set[str]) -> list[tuple[str, int]]:
+        if src in seen or src == 'X':
+            return [('', maxsize)]
+
+        if src == dst:
+            return [('A', 1)]
+
+        seen.add(src)
+
+        src_row, src_col = self.row_col_from_num_pad[src]
+
+        possible_this_level: list[tuple[str, int]] = []
+
+        for row_diff, col_diff, input in [(-1, 0, '^'), (0, 1, '>'), (1, 0, 'v'), (0, -1, '<')]:
+            check_row = src_row + row_diff
+            check_col = src_col + col_diff
+
+            if check_row < 0 or check_row >= self.NUMBER_PAD_ROWS or check_col < 0 or check_col >= self.NUMBER_PAD_COLS:
+                continue
+
+            this_move_dst = self.number_pad[check_row][check_col]
+
+            this_layer_res = self.number_pad_possible(this_move_dst, dst, seen)
+
+            for this_layer_seq, this_layer_dist in this_layer_res:
+                if this_layer_dist < maxsize:
+                    possible_this_level.append((input + this_layer_seq, 1 + this_layer_dist))
+
+        seen.remove(src)
+
+        return possible_this_level
+    
+    def number_pad_press(self, src: str, dst: str, levels: int) -> int:
+        if src not in self.dp:
+            self.dp[src] = {}
+        if dst in self.dp[src]:
+            return self.dp[src][dst]
         
-            lower_layers_seq, lower_layers_dist = lower_layers_seq + lower_layers_res[0], lower_layers_dist + lower_layers_res[1]
+        possible_ways = self.number_pad_possible(src, dst, set())
 
-        if lower_layers_dist < best_distance:
-            best_distance = lower_layers_dist
-            seq = lower_layers_seq
-        
-    dp[src][dst] = best_distance
-    return best_distance
+        best_distance = maxsize
+        seq = ''
 
+        for this_layer_seq, _ in possible_ways:
+            lower_layers_seq = ''
+            lower_layers_dist = 0
 
-codes: list[str] = []
-dp: dict[str, dict[str, int]] = {}
-dir_pad_dp: dict[int, dict[str, dict[str, tuple[str, int]]]] = {}
-dir_pad_same_level_dp: dict[str, dict[str, list[tuple[str, int]]]] = {}
+            for i in range(len(this_layer_seq)):
+                lower_dst = this_layer_seq[i]
+                if i == 0:
+                    lower_src = 'A'
+                else:
+                    lower_src = this_layer_seq[i - 1]
+                    
+                if 1 in self.dir_pad_dp and lower_src in self.dir_pad_dp[1] and lower_dst in self.dir_pad_dp[1][lower_src]:
+                    lower_layers_res = self.dir_pad_dp[1][lower_src][lower_dst]
+                else:
+                    lower_layers_res = self.dir_pad_all_levels(lower_src, lower_dst, 1, levels)
+            
+                lower_layers_seq, lower_layers_dist = lower_layers_seq + lower_layers_res[0], lower_layers_dist + lower_layers_res[1]
 
-def solve(direction_keypad_count: int) -> str:
-    with open(
-        'input.txt', encoding="utf-8"
-    ) as f:
-        line = f.readline()
+            if lower_layers_dist < best_distance:
+                best_distance = lower_layers_dist
+            
+        self.dp[src][dst] = best_distance
+        return best_distance
+    
+    def get_complexity(self, direction_keypad_count: int) -> str:
+        self.dp = {}
+        self.dir_pad_dp = {}
+        self.dir_pad_same_level_dp = {}
+        codes: list[str] = []
 
-
-        while line:
-            code = [c for c in line]
-
-            codes.append(code[:-1] if code[-1] == '\n' else code)
-
+        with open(
+            # join('src', 'd21', 'input.txt'), encoding="utf-8"
+            join('input.txt'), encoding="utf-8"
+        ) as f:
             line = f.readline()
 
 
-    res = 0
-    for code in codes:
-        # s = ''
-        l = 0
-        for i in range(len(code)):
-            if i == 0:
-                l += number_pad_press('A', code[i], direction_keypad_count)
-            else:
-                l += number_pad_press(code[i - 1], code[i], direction_keypad_count)
+            while line:
+                code = [c for c in line]
 
-        # print(f'code {code} s {s} len(s) {len(s)}')
-        print(f'code {code} l {l}')
+                codes.append(code[:-1] if code[-1] == '\n' else code)
 
-        numeric_of_code = int(''.join(code[:-1]))
-        res += (l * numeric_of_code)
-        # print(f'dp ---')
-        # print(dp)
-    
-    return str(res)
+                line = f.readline()
 
+        res = 0
+        for code in codes:
+            l = 0
+            for i in range(len(code)):
+                if i == 0:
+                    l += self.number_pad_press('A', code[i], direction_keypad_count)
+                else:
+                    l += self.number_pad_press(code[i - 1], code[i], direction_keypad_count)
 
-pt_1_res = solve(3)
-
-print(f'pt_1_res: {pt_1_res}')
-print(f'pt_2_res: {'TODO'}')
-
-
+            numeric_of_code = int(''.join(code[:-1]))
+            res += (l * numeric_of_code)
         
+        return str(res)
+
+    def solve(self) -> str:
+        pt_1_res = self.get_complexity(3)
+        print(f'pt_1_res: {pt_1_res}')
+        pt_2_res = self.get_complexity(26)
+        print(f'pt_2_res: {pt_2_res}')
 
 
-# print(r)
-
-    
+d = Day01()
+d.solve()    
