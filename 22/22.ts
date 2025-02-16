d44b2d900ef7a037cd31d9a73180d637837b8886
Mason Pimentel <me@masonpimentel.com>
2025-02-15 22:46:07 -0800

    Day 22 part 2 using TS

--- 22/22.ts
+++ 22/22.ts
@@ -10,51 +10,70 @@ class Day22 {
         return secret % BigInt(16777216);
     }
 
-// 15887950
-// 16495136
-// 527345
-// 704524
-// 1553684
-// 12683156
-// 11100544
-// 12249484
-// 7753432
-// 5908254
+    generateNewSecret(secret: bigint): bigint {
+        const mul64 = secret * BigInt(64);
+        secret = this.mix(secret, mul64);
+        secret = this.prune(secret);
 
+        const div32 = secret / BigInt(32);
+        secret = this.mix(secret, div32);
+        secret = this.prune(secret);
 
-// 1: 8685429
-// 10: 4700978
-// 100: 15273692
-// 2024: 8667524
-
-// 1: 8685429
-// 10: 4700978
-// 100: 15273692
-// 2024: 8667524
-
-// sum: 37327623
-
-// 1
-// 10
-// 100
-// 2024
+        const mul2048 = secret * BigInt(2048);
+        secret = this.mix(secret, mul2048);
+        
+        return this.prune(secret);
+    }
     
     ithSecret(secret: bigint, iterations: number): bigint {
         for (const _ of Array.from({ length: iterations })) {
-            const mul64 = secret * BigInt(64);
-            secret = this.mix(secret, mul64);
-            secret = this.prune(secret);
-    
-            const div32 = secret / BigInt(32);
-            secret = this.mix(secret, div32);
-            secret = this.prune(secret);
-    
-            const mul2048 = secret * BigInt(2048);
-            secret = this.mix(secret, mul2048);
-            secret = this.prune(secret);        
+            secret = this.generateNewSecret(secret);
         }
 
         return secret
+    }
+
+    getPriceFromSecret(secret: bigint): number {
+        return Number(secret % 10n);
+    }
+
+    getBananasFromBuyer(buyer: Record<string, number>, seq: string): number {
+        if (seq in buyer) return buyer[seq];
+
+        return 0;
+    }
+
+    sequencePrices(secret: bigint, iterations: number): Record<string, number> {
+        const res: Record<string, number> = {};
+        let curPrice = this.getPriceFromSecret(secret);
+        let curSeq: number[] = [];
+        
+        // iterations - 1 because the original secret counts as one
+        for (const _ of Array.from({ length: iterations - 1 })) {
+            secret = this.generateNewSecret(secret);
+            const newPrice = this.getPriceFromSecret(secret);
+            // console.log(`secret ${secret} newPrice ${newPrice} curPrice ${curPrice}`)
+            
+            const thisDiff = newPrice - curPrice;
+
+            curSeq.push(thisDiff);
+
+            if (curSeq.length > 4) {
+                curSeq.shift();
+            }
+
+            if (curSeq.length === 4) {
+                const seqStr = curSeq.toString();
+
+                if (!(seqStr in res)) {
+                    res[seqStr] = newPrice;
+                }
+            }
+
+            curPrice = newPrice;
+        }
+
+        return res;
     }
 
     async solve(): Promise<[string, string]> {
@@ -69,23 +88,9 @@ class Day22 {
         const lines: string[] = [];
         for await (const line of rl) { lines.push(line) };
 
-        // multiply secret by 64 -> mul_res
-        // mul_res mix into secret
-        // prune secret
-
-        // divide secret by 32 -> div_res
-        // div_res round down to nearest integer
-        // div_res mix into secret
-        // prune secret
-
-        // multiply secret by 2048 -> mul2_res
-        // mul2_res mix into secret
-        // prune secret
-
-        // mix: biwise xor
-        // prune: % 16777216
-
         this.ithSecret(BigInt(123), 10)
+
+        const buyers: Array<Record<string, number> > = [];
 
         // name this better
         let pt_1_res = BigInt(0);
@@ -94,6 +99,28 @@ class Day22 {
             pt_1_res += r
         }
         console.log(`pt_1_res: ${pt_1_res}`)
+
+        const allSeqs: Set<string> = new Set();
+        for (const initialPrice of lines) {
+            const buyer = this.sequencePrices(BigInt(initialPrice), 2000);
+            Object.keys(buyer).forEach((s) => allSeqs.add(s));
+            buyers.push(buyer);
+        }
+
+        let pt_2_res = 0;
+        let seqCount = 0;
+        for (const seq of allSeqs) {
+            let thisSeqRes = 0;
+            for (const buyer of buyers) {
+                thisSeqRes += this.getBananasFromBuyer(buyer, seq);
+            }
+          
+            pt_2_res = Math.max(pt_2_res, thisSeqRes);
+            seqCount++;
+        }
+
+        console.log(`pt_2_res: ${pt_2_res}`)
+
 
         return ['TODO', 'TODO']
     }
