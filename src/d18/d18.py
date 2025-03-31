f5f8cbc6d6200b25ee346d99a60a26d8b92960f8
Mason Pimentel <me@masonpimentel.com>
2025-03-31 19:34:45 +0000

    Refactor Day 18

--- src/d18/d18.py
+++ src/d18/d18.py
@@ -1,104 +1,106 @@
+"""Day 18"""
+
 from collections import deque
 from os.path import join
-from base.day import Day
+from concurrent.futures import ProcessPoolExecutor
+from base.day import Day, SolveInfo
+
 
 class Day18(Day):
-    def solve(self):
+    """RAM Run"""
+
+    # pylint: disable=R0914
+    def get_pt_1(
+        self, rows: int, cols: int, byte_count: int, bytes_l: list[tuple[int, int]]
+    ) -> int:
+        grid: list[list[str]] = [["."] * cols for _ in range(rows)]
+
+        for i in range(byte_count):
+            row, col = bytes_l[i]
+            grid[row][col] = "#"
+
+        q: deque[tuple[int, int, int]] = deque([(0, 0, 0)])
+        seen: set[tuple[int, int]] = set()
+
+        while len(q) > 0:
+            grid_row, grid_col, dis = q.popleft()
+
+            if grid_row < 0 or grid_row >= rows or grid_col < 0 or grid_col >= cols:
+                continue
+
+            if grid[grid_row][grid_col] == "#":
+                continue
+
+            seen_tpl = (grid_row, grid_col)
+            if seen_tpl in seen:
+                continue
+            seen.add(seen_tpl)
+
+            if grid_row == rows - 1 and grid_col == cols - 1:
+                return dis
+
+            for row_diff, col_diff in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                q.append((grid_row + row_diff, grid_col + col_diff, dis + 1))
+
+        return -1
+
+    # pylint: disable=R0914
+    def get_pt_2(self, rows: int, cols: int, bytes_l: list[tuple[int, int]]) -> str:
+        grid: list[list[str]] = [["."] * cols for _ in range(rows)]
+
+        for row, col in bytes_l:
+            grid[row][col] = "#"
+
+            q: deque[tuple[int, int, int]] = deque([(0, 0, 0)])
+            seen: set[tuple[int, int]] = set()
+
+            trapped = True
+            while len(q) > 0:
+                grid_row, grid_col, dis = q.popleft()
+
+                if grid_row < 0 or grid_row >= rows or grid_col < 0 or grid_col >= cols:
+                    continue
+
+                if grid[grid_row][grid_col] == "#":
+                    continue
+
+                seen_tpl = (grid_row, grid_col)
+                if seen_tpl in seen:
+                    continue
+                seen.add(seen_tpl)
+
+                if grid_row == rows - 1 and grid_col == cols - 1:
+                    trapped = False
+                    break
+
+                for row_diff, col_diff in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                    q.append((grid_row + row_diff, grid_col + col_diff, dis + 1))
+
+            if trapped:
+                r = f"{row},{col}"
+                return r
+
+        return ""
+
+    def solve(self) -> SolveInfo:
         with open(join("src", "d18", "input.txt"), encoding="utf-8") as f:
             line = f.readline()
 
-            bytes: list[tuple[int, int]] = []
+            bytes_l: list[tuple[int, int]] = []
 
             while line:
                 row = line[:-1] if line[-1] == "\n" else line[:]
 
                 s = row.split(",")
-                bytes.append((int(s[0]), int(s[1])))
+                bytes_l.append((int(s[0]), int(s[1])))
 
                 line = f.readline()
 
-            def part_1(rows: int, cols: int, byte_count: int) -> int:
-                grid: list[list[str]] = [["."] * cols for _ in range(rows)]
+        with ProcessPoolExecutor() as executor:
+            future1 = executor.submit(self.get_pt_1, 71, 71, 1024, bytes_l)
+            future2 = executor.submit(self.get_pt_2, 71, 71, bytes_l)
 
-                for i in range(byte_count):
-                    row, col = bytes[i]
+            pt_1_res = future1.result()
+            pt_2_res = future2.result()
 
-                    grid[row][col] = "#"
-
-                q: list[tuple[int, int, int]] = deque([(0, 0, 0)])
-                seen: set[str] = set()
-
-                while len(q) > 0:
-                    grid_row, grid_col, dis = q.popleft()
-
-                    if (
-                        grid_row < 0
-                        or grid_row >= rows
-                        or grid_col < 0
-                        or grid_col >= cols
-                    ):
-                        continue
-
-                    if grid[grid_row][grid_col] == "#":
-                        continue
-
-                    seen_tpl = (grid_row, grid_col)
-                    if seen_tpl in seen:
-                        continue
-                    seen.add(seen_tpl)
-
-                    if grid_row == rows - 1 and grid_col == cols - 1:
-                        return dis
-
-                    for row_diff, col_diff in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                        q.append((grid_row + row_diff, grid_col + col_diff, dis + 1))
-
-            def part_2(rows: int, cols: int):
-                grid: list[list[str]] = [["."] * cols for _ in range(rows)]
-
-                l = len(bytes)
-                for i in range(l):
-                    row, col = bytes[i]
-
-                    grid[row][col] = "#"
-
-                    q: list[tuple[int, int, int]] = deque([(0, 0, 0)])
-                    seen: set[str] = set()
-
-                    trapped = True
-                    while len(q) > 0:
-                        grid_row, grid_col, dis = q.popleft()
-
-                        if (
-                            grid_row < 0
-                            or grid_row >= rows
-                            or grid_col < 0
-                            or grid_col >= cols
-                        ):
-                            continue
-
-                        if grid[grid_row][grid_col] == "#":
-                            continue
-
-                        seen_tpl = (grid_row, grid_col)
-                        if seen_tpl in seen:
-                            continue
-                        seen.add(seen_tpl)
-
-                        if grid_row == rows - 1 and grid_col == cols - 1:
-                            trapped = False
-                            break
-
-                        for row_diff, col_diff in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                            q.append(
-                                (grid_row + row_diff, grid_col + col_diff, dis + 1)
-                            )
-
-                    if trapped:
-                        r = f"{row},{col}"
-                        return r
-
-            pt_1_res = part_1(71, 71, 1024)
-            pt_2_res = part_2(71, 71)
-
-            return (str(pt_1_res), pt_2_res)
+            return SolveInfo(str(pt_1_res), pt_2_res)
